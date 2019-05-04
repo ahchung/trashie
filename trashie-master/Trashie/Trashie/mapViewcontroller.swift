1cf643c31b936f5a49eaae7b985ae5520ac35018
srishtisan <sanyasrishti@gmail.com>
2019-05-04 12:10:41 -0400

    update location, link IBObjects:

--- trashie-master/Trashie/Trashie/mapViewcontroller.swift
+++ trashie-master/Trashie/Trashie/mapViewcontroller.swift
@@ -10,18 +10,42 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 
-class mapViewcontroller: UIViewController, MKMapViewDelegate {
+class mapViewcontroller: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
+    var currentLocation = CLLocation()
+    
+    
+    //set a default location in case the user doesn't consent
     let initialLocation = CLLocation(latitude: 40.730869, longitude: -73.997383)
     
+    //let initialLocation = CLLocation(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+    
+    //create a location manager to keep track of where you are
+    var locationManager = CLLocationManager()
+    
+    //area to search for locations (in m)
     let searchRadius: CLLocationDistance = 2000
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self
+        // mapView.delegate = self
+        
+        
+        // self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+            
+            mapView.showsUserLocation = true
+        }
+        
         let coordinateRegion = MKCoordinateRegion.init(center: initialLocation.coordinate, latitudinalMeters: searchRadius * 2.0, longitudinalMeters: searchRadius * 2.0)
         
         mapView.setRegion(coordinateRegion, animated: true)
@@ -30,11 +54,26 @@ class mapViewcontroller: UIViewController, MKMapViewDelegate {
         searchInMap()
     }
     
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0]
+        
+        
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        // let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+
+
+        mapView.setRegion(region, animated: true)
+    }
+    
     func searchInMap(){
         let request = MKLocalSearch.Request()
+       // request.naturalLanguageQuery = segControl.titleForSegment(at: segControl.selectedSegmentIndex)
         request.naturalLanguageQuery = segControl.titleForSegment(at: segControl.selectedSegmentIndex)
         
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        // let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         request.region = MKCoordinateRegion(center: initialLocation.coordinate, span: span)
         
         let search = MKLocalSearch(request: request)
